51a481041b015ffbf8f4508e6086166954c4b580
AlexCode12 <alexander.olsson2@elev.ga.ntig.se>
2020-10-06 13:50:00 +0200

    Glömde att lösa med git, repetition med section 4

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : SimpleWork 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140225
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="css/default.css" rel="stylesheet"/>
+<link href="css/fonts.css" rel="stylesheet"/>
+
+</head>
+<body>
+
+<div id="header-featured">
+		<div id="banner-wrapper">
+			<div id="banner" class="container">
+				<h2>Maecenas luctus lectus</h2>
+				<p>This is <strong>SimpleWork</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :) </p>
+				<a href="#" class="button">Etiam posuere</a> </div>
+		</div>
+	</div>
+</div>
+    @yield('content')
+</body>
+</html>
